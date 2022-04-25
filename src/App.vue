3702158bec893eb8d0e845967f57307e2f8a4cb0
Marcos Veloso <marcosvictor12345@gmail.com>
2022-04-24 23:41:01 -0300

    feat(primeiro commit): adicionando atomos e molecolas do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="component">
+    <Logo />
+    <NavBar />
+    <SearchForm />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue'
+
+import { Logo } from '@/components/atoms'
+import { NavBar } from '@/components/molecules'
+import { SearchForm } from '@/components/molecules'
 
 export default defineComponent({
-  name: "App",
   components: {
-    HelloWorld,
-  },
-});
+    Logo,
+    NavBar,
+    SearchForm
+  }
+})
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+.component {
+  margin: 5rem;
 }
 </style>
